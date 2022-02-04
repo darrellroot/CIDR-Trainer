bb2688763f669d2de8199278b9444e44d1dc915e
Darrell Root <darrellroot@mac.com>
2022-02-03 16:09:02 -0800

    Initial Commit

---
+++ Shared/CIDR_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIDR_TrainerApp.swift
+//  Shared
+//
+//  Created by Darrell Root on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CIDR_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
