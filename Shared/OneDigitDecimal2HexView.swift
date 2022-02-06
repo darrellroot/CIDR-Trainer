2c021319835a83acbc56caf625abf21068a9e7c8
Darrell Root <darrellroot@mac.com>
2022-02-05 22:27:09 -0800

    Big marker indicating correct or wrong works!

--- Shared/OneDigitDecimal2HexView.swift
+++ Shared/OneDigitDecimal2HexView.swift
@@ -26,6 +26,7 @@ struct OneDigitDecimal2HexView: View {
     @State var given: Int = Int.random(in: 0..<16)
     @State var lastResult = "Press your answer and hit the up arrow"
     @State var lastCorrect = true
+    @State var displayCheck = false
 
     var givenHex: String {
         return String(format: "0x%X",given)
@@ -38,6 +39,10 @@ struct OneDigitDecimal2HexView: View {
         coreGames.first?.wrong()
         
         lastResult = "Incorrect: \(given) in hex is \(givenHex) not 0x\(answer)"
+        displayCheck = true
+        withAnimation {
+            displayCheck = false
+        }
         newQuestion()
     }
     func correctAnswer() {
@@ -46,6 +51,10 @@ struct OneDigitDecimal2HexView: View {
         }
         coreGames.first?.correct()
         lastResult = "Correct: \(given) in hex is \(givenHex)"
+        displayCheck = true
+        withAnimation {
+            displayCheck = false
+        }
         newQuestion()
     }
     func submit() {
@@ -70,34 +79,36 @@ struct OneDigitDecimal2HexView: View {
         input = ""
     }
     var body: some View {
-        VStack(spacing: 10) {
-            Spacer()
-            List {
-                Section("Results") {
-                    Text("\(lastResult)")
-                        .foregroundColor(lastCorrect ? Color.green : Color.red)
-                        .fontWeight(.bold)
-                    //-1 means error getting core data
-                    Text("Recent \(GameScore.lastSize) score: \(coreGames.first?.last100correct ?? -1) correct \(coreGames.first?.last100wrong ?? -1) wrong")
-                    Text("All time score: \(coreGames.first?.correctTotal ?? -1) out of \((coreGames.first?.correctTotal ?? -1) + (coreGames.first?.wrongTotal ?? -1))")
-                }
-                Section("Next Task") {
-                    Text("Convert \(given) to Hex")
-                    Text(input)
-                }
-                .foregroundColor(Color.accentColor)
+        ZStack {
+            VStack {
+                List {
+                    Section("Results") {
+                        Text("\(lastResult)")
+                            .foregroundColor(lastCorrect ? Color.green : Color.red)
+                            .fontWeight(.bold)
+                        //-1 means error getting core data
+                        Text("Recent \(GameScore.lastSize) score: \(coreGames.first?.last100correct ?? -1) correct \(coreGames.first?.last100wrong ?? -1) wrong")
+                        Text("All time score: \(coreGames.first?.correctTotal ?? -1) out of \((coreGames.first?.correctTotal ?? -1) + (coreGames.first?.wrongTotal ?? -1))")
+                    }
+                    Section("Next Task") {
+                        Text("Convert \(given) to Hex")
+                        Text(input)
+                    }
+                    .foregroundColor(Color.accentColor)
 
-            }
-            Spacer()
-            HexKeyboardView(input: $input,submit: submit)
-        }.onDisappear {
-            do {
-                try moc.save()
-                print("Saved core data context")
-            } catch {
-                print("Failed to save core data context \(error.localizedDescription)")
-            }
-        }
+                }
+                Spacer()
+                HexKeyboardView(input: $input,submit: submit)
+            }.onDisappear {
+                do {
+                    try moc.save()
+                    print("Saved core data context")
+                } catch {
+                    print("Failed to save core data context \(error.localizedDescription)")
+                }
+            }//main vstack
+            Image(systemName: (lastCorrect ? "checkmark" : "x.circle")).font(.system(size: 150)).opacity(displayCheck ? 0.4 : 0.0)
+        }// zstack
 
         .navigationTitle("1 Digit Decimal -> Hex")
         .navigationBarTitleDisplayMode(.inline)
--- Shared/OneDigitHex2DecimalView.swift
+++ Shared/OneDigitHex2DecimalView.swift
@@ -25,6 +25,7 @@ struct OneDigitHex2DecimalView: View {
     @State var given: Int = Int.random(in: 0..<16)
     @State var lastResult = "Press your answer and hit the up arrow"
     @State var lastCorrect = true
+    @State var displayCheck = false
     
     var givenHex: String {
         return String(format: "0x%X",given)
@@ -40,6 +41,10 @@ struct OneDigitHex2DecimalView: View {
         } else {
             lastResult = "Incorrect: \(givenHex) in decimal is \(given)"
         }
+        displayCheck = true
+        withAnimation {
+            displayCheck = false
+        }
         newQuestion()
     }
     func correctAnswer() {
@@ -49,6 +54,10 @@ struct OneDigitHex2DecimalView: View {
         }
         coreGames.first?.correct()
         lastResult = "Correct: \(givenHex) in hex is \(given)"
+        displayCheck = true
+        withAnimation {
+            displayCheck = false
+        }
         newQuestion()
     }
     func submit() {
@@ -73,34 +82,36 @@ struct OneDigitHex2DecimalView: View {
         input = ""
     }
     var body: some View {
-        VStack(spacing: 10) {
-            Spacer()
-            List {
-                Section("Results") {
-                    Text("\(lastResult)")
-                        .foregroundColor(lastCorrect ? Color.green : Color.red)
-                        .fontWeight(.bold)
-                    //-1 means error getting core data
-                    Text("Recent \(GameScore.lastSize) score: \(coreGames.first?.last100correct ?? -1) correct \(coreGames.first?.last100wrong ?? -1) wrong")
-                    Text("All time score: \(coreGames.first?.correctTotal ?? -1) out of \((coreGames.first?.correctTotal ?? -1) + (coreGames.first?.wrongTotal ?? 0))")
-                }
-                Section("Next Task") {
-                    Text("Convert \(givenHex) to Decimal")
-                    Text(input)
-                }
-                .foregroundColor(Color.accentColor)
+        ZStack {
+            VStack {
+                List {
+                    Section("Results") {
+                        Text("\(lastResult)")
+                            .foregroundColor(lastCorrect ? Color.green : Color.red)
+                            .fontWeight(.bold)
+                        //-1 means error getting core data
+                        Text("Recent \(GameScore.lastSize) score: \(coreGames.first?.last100correct ?? -1) correct \(coreGames.first?.last100wrong ?? -1) wrong")
+                        Text("All time score: \(coreGames.first?.correctTotal ?? -1) out of \((coreGames.first?.correctTotal ?? -1) + (coreGames.first?.wrongTotal ?? 0))")
+                    }
+                    Section("Next Task") {
+                        Text("Convert \(givenHex) to Decimal")
+                        Text(input)
+                    }
+                    .foregroundColor(Color.accentColor)
 
-            }
-            Spacer()
-            DecimalKeyboardView(input: $input,submit: submit)
-        }.onDisappear {
-            do {
-                try moc.save()
-                print("Saved core data context")
-            } catch {
-                print("Failed to save core data context \(error.localizedDescription)")
-            }
-        }
+                }
+                Spacer()
+                DecimalKeyboardView(input: $input,submit: submit)
+            }.onDisappear {
+                do {
+                    try moc.save()
+                    print("Saved core data context")
+                } catch {
+                    print("Failed to save core data context \(error.localizedDescription)")
+                }
+            }// main vstack
+            Image(systemName: (lastCorrect ? "checkmark" : "x.circle")).font(.system(size: 150)).opacity(displayCheck ? 0.4 : 0.0)
+        }// zstack
 
         .navigationTitle("1 Digit Hex -> Decimal")
         .navigationBarTitleDisplayMode(.inline)
